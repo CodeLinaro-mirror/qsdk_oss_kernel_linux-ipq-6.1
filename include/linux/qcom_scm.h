38bef4df6db2e13a2cc181a9bc57f8085a49d8d8
Poovendhan Selvaraj <quic_poovendh@quicinc.com>
2023-04-30 21:36:01 +0530

    qcom : scm : Add scm call support for trybit and read dload register
    Change-Id: Ia00e0e3250a1d34b96dd401e3a39a75202b92945
    Signed-off-by: Poovendhan Selvaraj <quic_poovendh@quicinc.com>

--- include/linux/qcom_scm.h
+++ include/linux/qcom_scm.h
@@ -19,6 +19,7 @@
 #define QTI_TZ_QSEE_LOG_ENCR_ID		0x1
 #define QTI_TZ_LOG_NO_UPDATE		-6
 #define QTI_SCM_SVC_FUSE		0x8
+#define QTI_TRYBIT			BIT(12)
 
 struct qcom_scm_hdcp_req {
 	u32 addr;
@@ -175,4 +176,6 @@ extern int qcom_sec_upgrade_auth(unsigned int scm_cmd_id,
 extern int qcom_sec_upgrade_auth_meta_data(unsigned int scm_cmd_id,unsigned int sw_type,
 					   unsigned int img_size,unsigned int load_addr,
 					   void* hash_addr,unsigned int hash_size);
+extern int qcom_scm_enable_try_mode(void);
+extern int qcom_read_dload_reg(void);
 #endif
