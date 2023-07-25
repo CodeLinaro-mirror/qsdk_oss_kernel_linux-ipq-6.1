eb27f8ec25060816e1e1f73a1573e4da8bdec431
Luo Jie <quic_luoj@quicinc.com>
2023-07-25 15:41:17 +0800

    net: mdio: mdio-gpio: Add preinit private member
    Change-Id: I652b7ac272603674a148cd2c324ac7af8bfd13b3
    Signed-off-by: Luo Jie <quic_luoj@quicinc.com>

--- include/linux/mdio-bitbang.h
+++ include/linux/mdio-bitbang.h
@@ -36,6 +36,7 @@ struct mdiobb_ctrl {
 	unsigned int override_op_c22;
 	u8 op_c22_read;
 	u8 op_c22_write;
+	void (*preinit)(struct mii_bus *bus);
 };
 
 int mdiobb_read(struct mii_bus *bus, int phy, int reg);
