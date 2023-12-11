sricharanaz/kernel

net :ipv4/ipv6 : Delete multicast rule during mcproxy stop
During mcproxy stop, call the delete callback

Change-Id: Ia08a082f770a1a4b8443be187488ab814cf0ca26
Signed-off-by: Suman Ghosh <sumaghos@codeaurora.org>

net/ipv4/ipmr.c
@@ -1436,6 +1436,7 @@ static void mroute_clean_tables(struct mr_table *mrt, bool all)
 	int i;
 	LIST_HEAD(list);
 	struct mfc_cache *c, *next;
+	u32 origin, group;
 
 	/* Shut down all active vif entries */
 
@@ -1452,9 +1453,14 @@ static void mroute_clean_tables(struct mr_table *mrt, bool all)
 		list_for_each_entry_safe(c, next, &mrt->mfc_cache_array[i], list) {
 			if (!all && (c->mfc_flags & MFC_STATIC))
 				continue;
+			origin = c->mfc_origin;
+			group = c->mfc_mcastgrp;
 			list_del_rcu(&c->list);
 			mroute_netlink_event(mrt, c, RTM_DELROUTE);
 			ipmr_cache_free(c);
+
+			/* Inform offload modules of the delete event */
+			ipmr_sync_entry_delete(origin, group);
 		}
 	}
 

CodeLinaro-mirror/qsdk_oss_kernel_linux-ipq-6.1

net/ipv4/ipmr.c
@@ -1512,6 +1512,7 @@ static void mroute_clean_tables(struct mr_table *mrt, int flags)
 	struct net *net = read_pnet(&mrt->net);
 	struct mr_mfc *c, *tmp;
 	struct mfc_cache *cache;
+	u32 origin, group;
 	LIST_HEAD(list);
 	int i;
 
@@ -1536,10 +1537,14 @@ static void mroute_clean_tables(struct mr_table *mrt, int flags)
 			rhltable_remove(&mrt->mfc_hash, &c->mnode, ipmr_rht_params);
 			list_del_rcu(&c->list);
 			cache = (struct mfc_cache *)c;
+			origin = cache->mfc_origin;
+			group = cache->mfc_mcastgrp;
 			call_ipmr_mfc_entry_notifiers(net, FIB_EVENT_ENTRY_DEL, cache,
 						      mrt->id);
 			mroute_netlink_event(mrt, cache, RTM_DELROUTE);
 			mr_cache_put(c);
+			/* Inform offload modules of the delete event */
+			ipmr_sync_entry_delete(origin, group);
 		}
 	}
 